7bcccc8b789eb76eda97aa226f64da581c01ce20
MaFernandaLopeZ <maferlz265@gmail.com>
2023-10-23 22:05:05 -0600

    agregando entity, repository y properties

---
+++ src/main/java/com/service/employee/model/Employee.java
@@ -0,0 +1,35 @@
+package com.service.employee.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employee")
+@NoArgsConstructor @AllArgsConstructor
+@Getter
+@Setter
+public class Employee {
+	
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	
+	@Column(name = "first_name")
+	private String first_name;
+	
+	@Column(name = "last_name")
+	private String last_name;
+	
+	@Column(name = "email")
+	private String email;
+
+}

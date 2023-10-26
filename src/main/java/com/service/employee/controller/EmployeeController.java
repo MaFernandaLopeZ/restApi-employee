e19f5a3b1754cd403944a07145df532e3eec6b50
MaFernandaLopeZ <maferlz265@gmail.com>
2023-10-25 21:54:12 -0600

    agregando metodo de getEmployee(id)

--- src/main/java/com/service/employee/controller/EmployeeController.java
+++ src/main/java/com/service/employee/controller/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.service.employee.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +23,17 @@ public class EmployeeController {
 	@GetMapping("/employees")
 	public List<Employee> getAll(){
 		return repository.findAll();
+	}
+	
+	//Get one employee
+	@GetMapping("/employees/{id}")
+	public Optional<Employee> getEmployee(@PathVariable Integer id) throws Exception{
+		Optional<Employee> theEmployee = repository.findById(id);
+		
+		if(theEmployee.isEmpty())
+			throw new Exception("Employee id not found - "+id);
+		
+		return theEmployee;
 	}
 	
 
